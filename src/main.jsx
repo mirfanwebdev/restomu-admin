f1451c3252ae41fb2cfa6b269a18f65572a50de1
m.irfan <m.irfanwebdev@gmail.com>
2024-02-05 15:22:36 +0700

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import App from "./App.jsx";
 import "./index.css";
 import LoginPage from "./pages/LoginPage/index.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "login",
-    element: <LoginPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "login",
+      element: <LoginPage />,
+    },
+  ],
+  { basename: "/restomu-admin/" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
